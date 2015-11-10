9983465247f91fc3f4a99aa53570a155db229040
servando coria <cohs88@gmail.com>
2015-11-09 21:52:55 -0800

    creating the db-context

---
+++ CarRental.Data/CarRentalContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data.Entity;
+
+namespace CarRental.Data
+{
+    public class CarRentalContext : DbContext
+    {
+        public CarRentalContext():base("name=CarRental")
+        {
+            Database.SetInitializer<CarRentalContext>(null);
+        }
+    }
+}
