47a895a6e2e96300e04f3a92ea7b88a9e845877f
servando coria <cohs88@gmail.com>
2015-11-16 18:56:09 -0800

    reservation data repository added

---
+++ CarRental.Data.Contracts/RepositoryInterfaces/IReservationRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CarRental.Business.Entities;
+using Core.Common.Contracts;
+
+namespace CarRental.Data.Contracts.RepositoryInterfaces
+{
+    public interface IReservationRepository : IDataRepository<Reservation>
+    {
+        IEnumerable<Reservation> GetReservationsByPickupDate(DateTime pickupDate);
+        IEnumerable<CustomerReservationInfo> GetCurrentCustomerReservationInfo();
+        IEnumerable<CustomerReservationInfo> GetCustomerOpenReservationInfo(int accountId);
+    }
+}
---
+++ CarRental.Data/DataRepositories/ReservationRepository.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CarRental.Business.Entities;
+using Core.Common.Data;
+using CarRental.Data.Contracts.RepositoryInterfaces;
+using System.ComponentModel.Composition;
+using Core.Common.Contracts;
+using Core.Common.Extensions;
+using CarRental.Data.Contracts;
+
+namespace CarRental.Data.DataRepositories
+{
+    public class ReservationRepository : DataRepositoryBase<Reservation>, IReservationRepository
+    {
+        public IEnumerable<CustomerReservationInfo> GetCurrentCustomerReservationInfo()
+        {
+            using (CarRentalContext entityContext = new CarRentalContext())
+            {
+                var query = from r in entityContext.ReservationSet
+                            join a in entityContext.AccountSet on r.AccountId equals a.AccountId
+                            join c in entityContext.CarSet on r.CarId equals c.CarId
+                            select new CustomerReservationInfo()
+                            {
+                                Customer = a,
+                                Car = c,
+                                Reservation = r
+                            };
+
+                return query.ToFullyLoaded();
+            }
+        }
+
+        public IEnumerable<CustomerReservationInfo> GetCustomerOpenReservationInfo(int accountId)
+        {
+            using (CarRentalContext entityContext = new CarRentalContext())
+            {
+                var query = from r in entityContext.ReservationSet
+                            join a in entityContext.AccountSet on r.AccountId equals a.AccountId
+                            join c in entityContext.CarSet on r.CarId equals c.CarId
+                            where r.AccountId == accountId
+                            select new CustomerReservationInfo()
+                            {
+                                Customer = a,
+                                Car = c,
+                                Reservation = r
+                            };
+
+                return query.ToFullyLoaded();
+            }
+        }
+
+        public IEnumerable<Reservation> GetReservationsByPickupDate(DateTime pickupDate)
+        {
+            using (CarRentalContext entityContext = new CarRentalContext())
+            {
+                var query = from r in entityContext.ReservationSet
+                            where r.RentalDate < pickupDate
+                            select r;
+
+                return query.ToFullyLoaded();
+            }
+        }
+
+        protected override Reservation AddEntity(CarRentalContext entityContext, Reservation entity)
+        {
+            return entityContext.ReservationSet.Add(entity);
+        }
+
+        protected override IEnumerable<Reservation> GetEntities(CarRentalContext entityContext)
+        {
+            return from e in entityContext.ReservationSet
+                   select e;
+        }
+
+        protected override Reservation GetEntity(CarRentalContext entityContext, int id)
+        {
+            var query = (from e in entityContext.ReservationSet
+                   where e.ReservationId == id
+                   select e);
+
+            var results = query.FirstOrDefault();
+
+            return results;
+        }
+
+        protected override Reservation UpdateEntity(CarRentalContext entityContext, Reservation entity)
+        {
+            return (from e in entityContext.ReservationSet
+                    where e.ReservationId == entity.ReservationId
+                    select e).FirstOrDefault();
+        }
+    }
+}
