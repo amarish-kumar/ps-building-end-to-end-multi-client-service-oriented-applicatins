3eac0ee4ea7927afc4e7b131d1ed52fdecd6871b
servando coria <cohs88@gmail.com>
2015-11-16 18:46:17 -0800

    rental data repository added

---
+++ CarRental.Data.Contracts/RepositoryInterfaces/IRentalRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CarRental.Business.Entities;
+using Core.Common.Contracts;
+
+namespace CarRental.Data.Contracts.RepositoryInterfaces
+{
+    public interface IRentalRepository : IDataRepository<Rental>
+    {
+        IEnumerable<Rental> GetRentalHistoryByCar(int carId);
+        Rental GetCurrentRentalByCar(int carId);
+        IEnumerable<Rental> GetCurrentlyRentedCars();
+        IEnumerable<Rental> GetRentalHistoryByAccount(int accountId);
+        IEnumerable<CustomerRentalInfo> GetCurrentCustomerRentalInfo();
+    }
+}
