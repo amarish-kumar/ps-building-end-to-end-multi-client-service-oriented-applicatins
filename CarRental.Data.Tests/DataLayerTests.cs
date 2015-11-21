867983c22dc06dd58644128644886da84c045774
servando coria <cohs88@gmail.com>
2015-11-20 21:48:29 -0800

    repository usage and mocking

--- CarRental.Data.Tests/DataLayerTests.cs
+++ CarRental.Data.Tests/DataLayerTests.cs
@@ -11,6 +11,7 @@ using Core.Common.Contracts;
 using System.ComponentModel.Composition;
 using CarRental.Data.Contracts;
 using CarRental.Data.Contracts.RepositoryInterfaces;
+using Moq;
 
 namespace CarRental.Data.Tests
 {
@@ -21,6 +22,35 @@ namespace CarRental.Data.Tests
         public void Initialize()
         {
             ObjectBase.Container = MEFLoader.Init();
+        }
+
+        [TestMethod]
+        public void test_repository_usage()
+        {
+            RepositoryTestClass repositoryTest = new RepositoryTestClass();
+
+            IEnumerable<Car> cars = repositoryTest.GetCars();
+
+            Assert.IsTrue(cars != null);
+        }
+
+        [TestMethod]
+        public void test_repository_mocking()
+        {
+            List<Car> cars = new List<Car>()
+            {
+                new Car() { CarId = 1, Description = "Mustang" },
+                new Car() { CarId = 2, Description = "Corvette" }
+            };
+
+            Mock<ICarRepository> mockCarRepository = new Mock<ICarRepository>();
+            mockCarRepository.Setup(obj => obj.Get()).Returns(cars);
+
+            RepositoryTestClass repositoryTest = new RepositoryTestClass(mockCarRepository.Object);
+
+            IEnumerable<Car> ret = repositoryTest.GetCars();
+
+            Assert.IsTrue(ret == cars);
         }
     }
 
