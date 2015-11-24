a9dd4f01d2bcfd2e84a0d0c02f13086d81b9f59c
servando coria <cohs88@gmail.com>
2015-11-23 21:54:28 -0800

    repository usage and mocking

--- CarRental.Data.Tests/DataLayerTests.cs
+++ CarRental.Data.Tests/DataLayerTests.cs
@@ -35,6 +35,16 @@ namespace CarRental.Data.Tests
         }
 
         [TestMethod]
+        public void test_repository_factory_usage()
+        {
+            RepositoryFactoryTestClass factoryTest = new RepositoryFactoryTestClass();
+
+            IEnumerable<Car> cars = factoryTest.GetCars();
+
+            Assert.IsTrue(cars != null);
+        }
+
+        [TestMethod]
         public void test_repository_mocking()
         {
             List<Car> cars = new List<Car>()
@@ -49,6 +59,47 @@ namespace CarRental.Data.Tests
             RepositoryTestClass repositoryTest = new RepositoryTestClass(mockCarRepository.Object);
 
             IEnumerable<Car> ret = repositoryTest.GetCars();
+
+            Assert.IsTrue(ret == cars);
+        }
+
+        [TestMethod]
+        public void test_factory_mocking1()
+        {
+            List<Car> cars = new List<Car>()
+            {
+                new Car() { CarId = 1, Description = "Mustang" },
+                new Car() { CarId = 2, Description = "Corvette" }
+            };
+
+            Mock<IDataRepositoryFactory> mockDataRepository = new Mock<IDataRepositoryFactory>();
+            mockDataRepository.Setup(obj => obj.GetDataRepository<ICarRepository>().Get()).Returns(cars);
+
+            RepositoryFactoryTestClass factoryTest = new RepositoryFactoryTestClass(mockDataRepository.Object);
+
+            IEnumerable<Car> ret = factoryTest.GetCars();
+
+            Assert.IsTrue(ret == cars);
+        }
+
+        [TestMethod]
+        public void test_factory_mocking2()
+        {
+            List<Car> cars = new List<Car>()
+            {
+                new Car() { CarId = 1, Description = "Mustang" },
+                new Car() { CarId = 2, Description = "Corvette" }
+            };
+
+            Mock<ICarRepository> mockCarRepository = new Mock<ICarRepository>();
+            mockCarRepository.Setup(obj => obj.Get()).Returns(cars);
+
+            Mock<IDataRepositoryFactory> mockDataRepository = new Mock<IDataRepositoryFactory>();
+            mockDataRepository.Setup(obj => obj.GetDataRepository<ICarRepository>()).Returns(mockCarRepository.Object);
+
+            RepositoryFactoryTestClass factoryTest = new RepositoryFactoryTestClass(mockDataRepository.Object);
+
+            IEnumerable<Car> ret = factoryTest.GetCars();
 
             Assert.IsTrue(ret == cars);
         }
@@ -72,6 +123,31 @@ namespace CarRental.Data.Tests
         public IEnumerable<Car> GetCars()
         {
             IEnumerable<Car> cars = _CarRepository.Get();
+
+            return cars;
+        }
+    }
+
+    public class RepositoryFactoryTestClass
+    {
+        public RepositoryFactoryTestClass()
+        {
+            ObjectBase.Container.SatisfyImportsOnce(this);
+        }
+
+        public RepositoryFactoryTestClass(IDataRepositoryFactory dataRepositoryFactory)
+        {
+            _DataRepositoryFactory = dataRepositoryFactory;
+        }
+
+        [Import]
+        IDataRepositoryFactory _DataRepositoryFactory;
+
+        public IEnumerable<Car> GetCars()
+        {
+            ICarRepository carRepository = _DataRepositoryFactory.GetDataRepository<ICarRepository>();
+
+            IEnumerable<Car> cars = carRepository.Get();
 
             return cars;
         }
---
+++ CarRental.Data/DataRepositoryFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Core.Common.Contracts;
+using Core.Common.Core;
+using System.ComponentModel.Composition;
+
+namespace CarRental.Data.Contracts
+{
+    [Export(typeof(IDataRepositoryFactory))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class DataRepositoryFactory : IDataRepositoryFactory
+    {
+        public T GetDataRepository<T>() where T : IDataRepository
+        {
+            return ObjectBase.Container.GetExportedValue<T>();
+        }
+    }
+}
