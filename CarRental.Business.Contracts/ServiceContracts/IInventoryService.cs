b68868b7b17944c4449c78e2ac14b37f91f6e75f
servando coria <cohs88@gmail.com>
2015-11-26 20:49:03 -0800

    inventory service contract

---
+++ CarRental.Business.Contracts/ServiceContracts/IInventoryService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ServiceModel;
+using CarRental.Business.Entities;
+
+namespace CarRental.Business.Contracts
+{
+    [ServiceContract]
+    public interface IInventoryService
+    {
+        [OperationContract]
+        Car GetCar(int carId);
+
+        [OperationContract]
+        Car[] GetAllCars();
+    }
+}
