4000879c173ff5d37e61f078716e30b020ccf79c
servando coria <cohs88@gmail.com>
2015-11-29 11:00:01 -0800

    settig up the inventory service

---
+++ CarRental.Business.Managers/Managers/InventoryManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Core.Common.Contracts;
+using Core.Common.Core;
+using CarRental.Business.Contracts;
+using System.ComponentModel.Composition;
+using CarRental.Business.Entities;
+
+namespace CarRental.Business.Managers
+{
+    public class InventoryManager : IInventoryService
+    {
+        public InventoryManager()
+        {
+            ObjectBase.Container.SatisfyImportsOnce(this);
+        }
+
+        public InventoryManager(IDataRepositoryFactory dataRepositoryFactory)
+        {
+            _dataRepositoryFactory = dataRepositoryFactory;
+        }
+
+        [Import]
+        IDataRepositoryFactory _dataRepositoryFactory;
+
+        public Car GetCar(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car[] GetAllCars()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
