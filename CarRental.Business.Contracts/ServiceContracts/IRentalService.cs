71ab4b5610a3b6eb0be5348085646325608a3d52
servando coria <cohs88@gmail.com>
2016-01-12 21:40:10 -0800

    setting up the rental service

---
+++ CarRental.Business.Contracts/ServiceContracts/IRentalService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ServiceModel;
+using CarRental.Business.Entities;
+using Core.Common.Exceptions;
+
+namespace CarRental.Business.Contracts.ServiceContracts
+{
+    [ServiceContract]
+    public interface IRentalService
+    {
+        [OperationContract]
+        IEnumerable<Rental> GetRentalHistory(string loginEmail);
+    }
+}
---
+++ CarRental.Business.Managers/Managers/RentalManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CarRental.Business.Contracts.ServiceContracts;
+using CarRental.Business.Entities;
+using Core.Common.Contracts;
+using System.ComponentModel.Composition;
+using System.ServiceModel;
+
+namespace CarRental.Business.Managers.Managers
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall, ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class RentalManager : ManagerBase, IRentalService
+    {
+        [Import]
+        IDataRepositoryFactory _dataRepositoryFactory;
+        [Import]
+        IBusinessEngineFactory _businessEngineFactory;
+
+        public RentalManager()
+        {
+
+        }
+
+        public RentalManager(IDataRepositoryFactory dataRepositoryFactory)
+        {
+            _dataRepositoryFactory = dataRepositoryFactory;
+        }
+
+        public RentalManager(IBusinessEngineFactory businessEngineFactory)
+        {
+            _businessEngineFactory = businessEngineFactory;
+        }
+
+        public RentalManager(IDataRepositoryFactory dataRepositoryFactory, IBusinessEngineFactory businessEngineFactory)
+        {
+            _dataRepositoryFactory = dataRepositoryFactory;
+            _businessEngineFactory = businessEngineFactory;
+        }
+
+        /*
+        public RentalManager()
+        {
+
+        }
+       */
+
+        public IEnumerable<Rental> GetRentalHistory(string loginEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
