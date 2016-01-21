06628deec3fbaec6575fc94e1f18b98f0aa41202
servando coria <cohs88@gmail.com>
2016-01-20 22:16:31 -0800

    finishing the car rental engine

--- CarRental.Business.Contracts/ServiceContracts/IRentalService.cs
+++ CarRental.Business.Contracts/ServiceContracts/IRentalService.cs
@@ -7,14 +7,73 @@ using System.ServiceModel;
 using CarRental.Business.Entities;
 using Core.Common.Exceptions;
 using CarRental.Common;
+using CarRental.Business.Contracts.DataContracts;
 
 namespace CarRental.Business.Contracts.ServiceContracts
 {
     [ServiceContract]
     public interface IRentalService
     {
+        /*
+        [OperationContract(Name = "RentCarToCustomerImmediately")]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        [FaultContract(typeof(NotFoundException))]
+        [FaultContract(typeof(CarCurrentlyRentedException))]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        Rental RentCarToCustomer(string loginEmail, int carId, DateTime dateDueBack);
+
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        [FaultContract(typeof(NotFoundException))]
+        [FaultContract(typeof(CarCurrentlyRentedException))]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        Rental RentCarToCustomer(string loginEmail, int carId, DateTime rentalDate, DateTime dateDueBack);
+
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void AcceptCarReturn(int carId);
+        */
+
         [OperationContract]
         [FaultContract(typeof(AuthorizationValidationException))]
         IEnumerable<Rental> GetRentalHistory(string loginEmail);
+
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void CancelReservation(int reservationId);
+
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        CustomerReservationData[] GetCurrentReservations();
+
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        CustomerReservationData[] GetCustomerReservations(string loginEmail);
+
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        Rental GetRental(int rentalId);
+
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        CustomerRentalData[] GetCurrentRentals();
+
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        Reservation[] GetDeadReservations();
+
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        bool IsCarCurrentlyRented(int carId);
     }
 }
--- CarRental.Business.Managers/Managers/RentalManager.cs
+++ CarRental.Business.Managers/Managers/RentalManager.cs
@@ -12,6 +12,9 @@ using CarRental.Data.Contracts.RepositoryInterfaces;
 using Core.Common.Exceptions;
 using System.Security.Permissions;
 using CarRental.Common;
+using CarRental.Business.Contracts.DataContracts;
+using CarRental.Data.Contracts;
+using CarRental.Business.Common;
 
 namespace CarRental.Business.Managers.Managers
 {
@@ -49,7 +52,10 @@ namespace CarRental.Business.Managers.Managers
             IAccountRepository accountRepository = _dataRepositoryFactory.GetDataRepository<IAccountRepository>();
             Account authAcct = accountRepository.GetLogin(loginName);
             if (authAcct == null)
-                throw new NotFoundException(string.Format("Cannot find account for login name {0} to use for security trimming", loginName));
+            {
+                NotFoundException ex = new NotFoundException(string.Format("Cannot find account for login name {0} to use for security trimming", loginName));
+                throw new FaultException<NotFoundException>(ex, ex.Message);
+            }
 
             return authAcct;
         }
@@ -74,6 +80,147 @@ namespace CarRental.Business.Managers.Managers
 
                 return rentalHistory;
 
+            });
+        }
+
+        [OperationBehavior(TransactionScopeRequired = true)]
+        public void CancelReservation(int reservationId)
+        {
+            ExecuteFaultHandledOperation(() =>
+            {
+                IReservationRepository reservationRepository = _dataRepositoryFactory.GetDataRepository<IReservationRepository>();
+
+                Reservation reservation = reservationRepository.Get(reservationId);
+                if (reservation == null)
+                {
+                    NotFoundException ex = new NotFoundException(string.Format("No reservation found found for ID '{0}'.", reservationId));
+                    throw new FaultException<NotFoundException>(ex, ex.Message);
+                }
+
+                reservationRepository.Remove(reservationId);
+            });
+        }
+
+        public CustomerReservationData[] GetCurrentReservations()
+        {
+            return ExecuteFaultHandledOperation(() =>
+            {
+                IReservationRepository reservationRepository = _dataRepositoryFactory.GetDataRepository<IReservationRepository>();
+
+                List<CustomerReservationData> reservationData = new List<CustomerReservationData>();
+
+                IEnumerable<CustomerReservationInfo> reservationInfoSet = reservationRepository.GetCurrentCustomerReservationInfo();
+                foreach (CustomerReservationInfo reservationInfo in reservationInfoSet)
+                {
+                    reservationData.Add(new CustomerReservationData()
+                    {
+                        ReservationId = reservationInfo.Reservation.ReservationId,
+                        Car = reservationInfo.Car.Color + " " + reservationInfo.Car.Year + " " + reservationInfo.Car.Description,
+                        CustomerName = reservationInfo.Customer.FirstName + " " + reservationInfo.Customer.LastName,
+                        RentalDate = reservationInfo.Reservation.RentalDate,
+                        ReturnDate = reservationInfo.Reservation.ReturnDate
+                    });
+                }
+
+                return reservationData.ToArray();
+            });
+        }
+
+        public CustomerReservationData[] GetCustomerReservations(string loginEmail)
+        {
+            return ExecuteFaultHandledOperation(() =>
+            {
+                IAccountRepository accountRepository = _dataRepositoryFactory.GetDataRepository<IAccountRepository>();
+                IReservationRepository reservationRepository = _dataRepositoryFactory.GetDataRepository<IReservationRepository>();
+
+                Account account = accountRepository.GetLogin(loginEmail);
+                if (account == null)
+                {
+                    NotFoundException ex = new NotFoundException(string.Format("No account found for login '{0}'.", loginEmail));
+                    throw new FaultException<NotFoundException>(ex, ex.Message);
+                }
+
+                List<CustomerReservationData> reservationData = new List<CustomerReservationData>();
+
+                IEnumerable<CustomerReservationInfo> reservationInfoSet = reservationRepository.GetCustomerOpenReservationInfo(account.AccountId);
+                foreach (CustomerReservationInfo reservationInfo in reservationInfoSet)
+                {
+                    reservationData.Add(new CustomerReservationData()
+                    {
+                        ReservationId = reservationInfo.Reservation.ReservationId,
+                        Car = reservationInfo.Car.Color + " " + reservationInfo.Car.Year + " " + reservationInfo.Car.Description,
+                        CustomerName = reservationInfo.Customer.FirstName + " " + reservationInfo.Customer.LastName,
+                        RentalDate = reservationInfo.Reservation.RentalDate,
+                        ReturnDate = reservationInfo.Reservation.ReturnDate
+                    });
+                }
+
+                return reservationData.ToArray();
+            });
+        }
+
+        public Rental GetRental(int rentalId)
+        {
+            return ExecuteFaultHandledOperation(() =>
+            {
+                IAccountRepository accountRepository = _dataRepositoryFactory.GetDataRepository<IAccountRepository>();
+                IRentalRepository rentalRepository = _dataRepositoryFactory.GetDataRepository<IRentalRepository>();
+
+                Rental rental = rentalRepository.Get(rentalId);
+                if (rental == null)
+                {
+                    NotFoundException ex = new NotFoundException(string.Format("No rental record found for id '{0}'.", rentalId));
+                    throw new FaultException<NotFoundException>(ex, ex.Message);
+                }
+
+                return rental;
+            });
+        }
+
+        public CustomerRentalData[] GetCurrentRentals()
+        {
+            return ExecuteFaultHandledOperation(() =>
+            {
+                IRentalRepository rentalRepository = _dataRepositoryFactory.GetDataRepository<IRentalRepository>();
+
+                List<CustomerRentalData> rentalData = new List<CustomerRentalData>();
+
+                IEnumerable<CustomerRentalInfo> rentalInfoSet = rentalRepository.GetCurrentCustomerRentalInfo();
+                foreach (CustomerRentalInfo rentalInfo in rentalInfoSet)
+                {
+                    rentalData.Add(new CustomerRentalData()
+                    {
+                        RentalId = rentalInfo.Rental.RentalId,
+                        Car = rentalInfo.Car.Color + " " + rentalInfo.Car.Year + " " + rentalInfo.Car.Description,
+                        CustomerName = rentalInfo.Customer.FirstName + " " + rentalInfo.Customer.LastName,
+                        DateRented = rentalInfo.Rental.DateRented,
+                        ExpectedReturn = rentalInfo.Rental.DateDue
+                    });
+                }
+
+                return rentalData.ToArray();
+            });
+        }
+
+        public Reservation[] GetDeadReservations()
+        {
+            return ExecuteFaultHandledOperation(() =>
+            {
+                IReservationRepository reservationRepository = _dataRepositoryFactory.GetDataRepository<IReservationRepository>();
+
+                IEnumerable<Reservation> reservations = reservationRepository.GetReservationsByPickupDate(DateTime.Now.AddDays(-1));
+
+                return (reservations != null ? reservations.ToArray() : null);
+            });
+        }
+
+        public bool IsCarCurrentlyRented(int carId)
+        {
+            return ExecuteFaultHandledOperation(() =>
+            {
+                ICarRentalEngine carRentalEngine = _businessEngineFactory.GetBusinessEngine<ICarRentalEngine>();
+
+                return carRentalEngine.IsCarCurrentlyRented(carId);
             });
         }
     }
