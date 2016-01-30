51ec9b0eae4990beef3d3d8213ef833af2f5da52
servando coria <cohs88@gmail.com>
2016-01-30 11:51:42 -0800

    testing the manager

---
+++ CarRental.Business.Managers.Tests/InventoryManagerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CarRental.Business.Entities;
+using Moq;
+using Core.Common.Contracts;
+using CarRental.Data.Contracts.RepositoryInterfaces;
+
+namespace CarRental.Business.Managers.Tests
+{
+    [TestClass]
+    public class InventoryManagerTests
+    {
+        [TestMethod]
+        public void UpdateCar_add_new()
+        {
+            Car newCar = new Car();
+            Car addedCar = new Car() { CarId = 1 };
+
+            Mock<IDataRepositoryFactory> mockRepositoryFactory = new Mock<IDataRepositoryFactory>();
+            mockRepositoryFactory.Setup(obj => obj.GetDataRepository<ICarRepository>().Add(newCar)).Returns(addedCar);
+
+            InventoryManager manager = new InventoryManager(mockRepositoryFactory.Object);
+
+            Car results = manager.Update(newCar);
+
+            Assert.IsTrue(results == addedCar);
+        }
+
+        [TestMethod]
+        public void UpdateCar_update_existing()
+        {
+            Car existingCar = new Car() { CarId = 1 };
+            Car updatedCar = new Car() { CarId = 1 };
+
+
+            Mock<IDataRepositoryFactory> mockRepositoryFactory = new Mock<IDataRepositoryFactory>();
+            mockRepositoryFactory.Setup(obj => obj.GetDataRepository<ICarRepository>().Update(existingCar)).Returns(updatedCar);
+
+            InventoryManager manager = new InventoryManager(mockRepositoryFactory.Object);
+
+            Car results = manager.Update(existingCar);
+
+            Assert.IsTrue(results == updatedCar);
+        }
+    }
+}
