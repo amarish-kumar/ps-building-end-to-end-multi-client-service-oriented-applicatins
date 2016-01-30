e0633c1427329093ba48457218606a6794ab0fbf
servando coria <cohs88@gmail.com>
2016-01-30 10:29:43 -0800

    Wrapping up with the account manager

---
+++ CarRental.Business.Managers/Managers/AccountManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CarRental.Business.Contracts;
+using System.ServiceModel;
+using CarRental.Business.Entities;
+using Core.Common.Contracts;
+using System.ComponentModel.Composition;
+using CarRental.Data.Contracts.RepositoryInterfaces;
+using Core.Common.Exceptions;
+using System.Security.Permissions;
+using CarRental.Common;
+
+namespace CarRental.Business.Managers.Managers
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall,
+                    ConcurrencyMode = ConcurrencyMode.Multiple,
+                    ReleaseServiceInstanceOnTransactionComplete = false)]
+    public class AccountManager : ManagerBase, IAccountService
+    {
+        [Import]
+        IDataRepositoryFactory _DataRepositoryFactory;
+
+        public AccountManager()
+        {
+
+        }
+
+        public AccountManager(IDataRepositoryFactory dataRepositoryFactory)
+        {
+            _DataRepositoryFactory = dataRepositoryFactory;
+        }
+
+        protected override Account LoadAuthorizationValidationAccount(string loginName)
+        {
+            IAccountRepository accountRepository = _DataRepositoryFactory.GetDataRepository<IAccountRepository>();
+            Account authAcct = accountRepository.GetByLogin(loginName);
+            if (authAcct == null)
+            {
+                NotFoundException ex = new NotFoundException(string.Format("Cannot find account for login name {0} to use security", loginName));
+                throw new FaultException<NotFoundException>(ex, ex.Message);
+            }
+
+            return authAcct;
+        }
+
+        [PrincipalPermission(SecurityAction.Demand, Role = Security.CarRentalAdminRole)]
+        [PrincipalPermission(SecurityAction.Demand, Role = Security.CarRentalUser)]
+        public Account GetCustomerAccountInfo(string loginEmail)
+        {
+            return ExecuteFaultHandledOperation(() =>
+            {
+                IAccountRepository accountRepository = _DataRepositoryFactory.GetDataRepository<IAccountRepository>();
+
+                Account accountEntity = accountRepository.GetByLogin(loginEmail);
+                if (accountEntity == null)
+                {
+                    NotFoundException ex = new NotFoundException(string.Format("Account with login {0} is not in database", loginEmail));
+                    throw new FaultException<NotFoundException>(ex, ex.Message);
+                }
+
+                ValidateAuthorization(accountEntity);
+
+                return accountEntity;
+            });
+        }
+
+        [OperationBehavior(TransactionScopeRequired = true)]
+        [PrincipalPermission(SecurityAction.Demand, Role = Security.CarRentalAdminRole)]
+        [PrincipalPermission(SecurityAction.Demand, Role = Security.CarRentalUser)]
+        public void UpdateCustomerAccountInfo(Account account)
+        {
+            ExecuteFaultHandledOperation(() =>
+            {
+                IAccountRepository accountRepository = _DataRepositoryFactory.GetDataRepository<IAccountRepository>();
+
+                ValidateAuthorization(account);
+
+                Account updatedAccount = accountRepository.Update(account);
+            });
+        }
+    }
+}
