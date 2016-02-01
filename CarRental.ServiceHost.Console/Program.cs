0edd1db9bd52bbaf3bea56389f917cd2c9631f5d
servando coria <cohs88@gmail.com>
2016-02-01 15:47:27 -0800

    simple host setup of inventory service

---
+++ CarRental.ServiceHost.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Timers;
+using CarRental.Business.Bootstrapper;
+using CarRental.Business.Entities;
+using CarRental.Business.Managers;
+using Core.Common;
+using SM = System.ServiceModel;
+
+namespace CarRental.ServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting up services...");
+            Console.WriteLine("");
+
+
+            SM.ServiceHost host = new System.ServiceModel.ServiceHost(typeof(InventoryManager));
+            host.Open();
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Press [Enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
