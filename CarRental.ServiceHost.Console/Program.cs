55dfe90c777645e815da1ae864acb5465353bf43
servando coria <cohs88@gmail.com>
2016-02-01 16:15:14 -0800

    reporting hosting to console

--- CarRental.ServiceHost.Console/Program.cs
+++ CarRental.ServiceHost.Console/Program.cs
@@ -10,6 +10,7 @@ using CarRental.Business.Entities;
 using CarRental.Business.Managers;
 using Core.Common;
 using SM = System.ServiceModel;
+using CarRental.Business.Managers.Managers;
 
 namespace CarRental.ServiceHost
 {
@@ -21,13 +22,46 @@ namespace CarRental.ServiceHost
             Console.WriteLine("");
 
 
-            SM.ServiceHost host = new System.ServiceModel.ServiceHost(typeof(InventoryManager));
-            host.Open();
+            SM.ServiceHost hostInventoryManager = new System.ServiceModel.ServiceHost(typeof(InventoryManager));
+            SM.ServiceHost hostRentalManager = new System.ServiceModel.ServiceHost(typeof(RentalManager));
+            SM.ServiceHost hostAccountManager = new System.ServiceModel.ServiceHost(typeof(AccountManager));
 
+            StartService(hostInventoryManager, "InventoryManager");
+            StartService(hostRentalManager, "RentalManager");
+            StartService(hostAccountManager, "AccountManager");
 
             Console.WriteLine("");
             Console.WriteLine("Press [Enter] to exit.");
             Console.ReadLine();
+
+            StopService(hostInventoryManager, "InventoryManager");
+            StopService(hostRentalManager, "RentalManager");
+            StopService(hostAccountManager, "AccountManager");
+        }
+
+        static void StopService(SM.ServiceHost host, string serviceDescription)
+        {
+            host.Close();
+
+            Console.WriteLine("Service {0} stopped", serviceDescription);
+        }
+
+
+
+        static void StartService(SM.ServiceHost host, string serviceDescription)
+        {
+            host.Open();
+            Console.WriteLine("Service {0} started.", serviceDescription);
+
+            foreach (var endPoint in host.Description.Endpoints)
+            {
+                Console.WriteLine(string.Format("Listening on endpoits:"));
+                Console.WriteLine(string.Format("Address: {0}", endPoint.Address.Uri));
+                Console.WriteLine(string.Format("Binding: {0}", endPoint.Binding.Name));
+                Console.WriteLine(string.Format("Contract: {0}", endPoint.Contract.Name));
+            }
+
+            Console.WriteLine();
         }
     }
 }
