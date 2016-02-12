6f42b87ba3e1e6016fca83c6c467772eee76ea2b
servando coria <cohs88@gmail.com>
2016-02-11 20:21:02 -0800

    added missing decorator for businessnenginefactory, added ef to servicehost console

--- CarRental.Business/BusinessEngineFactory.cs
+++ CarRental.Business/BusinessEngineFactory.cs
@@ -5,9 +5,12 @@ using System.Text;
 
 using Core.Common.Contracts;
 using Core.Common.Core;
+using System.ComponentModel.Composition;
 
 namespace CarRental.Business
 {
+    [Export(typeof(IBusinessEngineFactory))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
     public class BusinessEngineFactory : IBusinessEngineFactory
     {
         public T GetBusinessEngine<T>() where T : IBusinessEngine
