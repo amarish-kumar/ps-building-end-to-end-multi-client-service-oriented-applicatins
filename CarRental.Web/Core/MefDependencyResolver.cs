22cc2323c1ff57ddeb9f9b3da09fda670079e93b
servando coria <cohs88@gmail.com>
2016-02-26 22:31:31 -0800

    mef bootstrapping and the dependency resolver

---
+++ CarRental.Web/Core/MefDependencyResolver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+using System.ComponentModel.Composition.Hosting;
+using Core.Common.Extensions;
+
+namespace CarRental.Web.Core
+{
+    public class MefDependencyResolver : IDependencyResolver
+    {
+        CompositionContainer _Container;
+        public MefDependencyResolver(CompositionContainer container)
+        {
+            _Container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _Container.GetExportedValueByType(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _Container.GetExportedValuesByType(serviceType);
+        }
+    }
+}
---
+++ CarRental.Web/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Http;
+
+using System.ComponentModel.Composition.Hosting;
+using System.Reflection;
+using CarRental.Client.Bootstrapper;
+using CarRental.Web.Core;
+
+namespace CarRental.Web
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            AggregateCatalog catalog = new AggregateCatalog();
+            catalog.Catalogs.Add(new AssemblyCatalog(Assembly.GetExecutingAssembly()));
+            CompositionContainer container = MEFLoader.Init(catalog.Catalogs);
+
+            DependencyResolver.SetResolver(new MefDependencyResolver(container));
+        }
+    }
+}
