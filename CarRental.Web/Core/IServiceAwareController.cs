b94186622f577a08501dd1264e4f221754fae076
servando coria <cohs88@gmail.com>
2016-03-04 22:07:00 -0800

    describing the layout view

---
+++ CarRental.Web/Core/IServiceAwareController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Core.Common.Contracts;
+
+namespace CarRental.Web.Core
+{
+    public interface IServiceAwareController
+    {
+        void RegisterDisposableServices(List<IServiceContract> disposableServices);
+
+        List<IServiceContract> DisposableServices { get; }
+    }
+}
