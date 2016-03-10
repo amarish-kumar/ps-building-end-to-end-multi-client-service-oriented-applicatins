90423ae09065783269a804653140ec86ea7ec855
servando coria <cohs88@gmail.com>
2016-03-09 21:33:20 -0800

    the account controller setup

---
+++ CarRental.Web/Controllers/MVC/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using CarRental.Web.Core;
+using System.ComponentModel.Composition;
+
+namespace CarRental.Web.Controllers.MVC
+{
+    [Export]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    [RoutePrefix("account")]
+    public class AccountController : ViewControllerBase
+    {
+        ISecurityAdapter _SecurityAdapter;
+
+        [ImportingConstructor]
+        public AccountController(ISecurityAdapter SecurityAdapter)
+        {
+            _SecurityAdapter = SecurityAdapter;
+        }
+
+        [HttpGet]
+        [Route("login")]// acount/login
+        public ActionResult Login()
+        {
+            _SecurityAdapter.Initialize();
+            return View();
+        }
+    }
+}
---
+++ CarRental.Web/Controllers/MVC/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.ComponentModel.Composition;
+
+namespace CarRental.Web.Controllers.MVC
+{
+    [Export]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    [RoutePrefix("home")]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("my")]
+        [Authorize]
+        public ActionResult MyAccount()
+        {
+            return View();
+        }
+    }
+}
